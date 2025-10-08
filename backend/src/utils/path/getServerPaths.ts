583041f73ab8932c55d993264d42606a5a781a6a
Raphael Goulart <contact@raphaelgoul.art>
2025-10-08 20:00:54 -0300

    backend: get FILE_ROOT from env

--- backend/src/utils/path/getServerPaths.ts
+++ backend/src/utils/path/getServerPaths.ts
@@ -1,10 +1,10 @@
 import { pathLikeToDirPath, resolve } from 'node-lib'
 import { isDev } from '../checkers/isDev'
 
-export const getServerRoot = () => pathLikeToDirPath(resolve(import.meta.dirname, '../../../'))
+export const getServerRoot = () => pathLikeToDirPath(process.env.FILE_ROOT ? process.env.FILE_ROOT : resolve(import.meta.dirname, '../../../'))
 export const getServerTemp = () => getServerRoot().gotoDir('temp')
 export const getServerFile = () => getServerRoot().gotoDir('files')
 export const getValidatorPath = () => {
-    const extension = process.platform === "win32" ? ".exe" : ".dll"
-    return getServerRoot().gotoFile(isDev() ? `../YARGReplayValidator/bin/Debug/net8.0/YARGReplayValidator${extension}` : `bin/YARGReplayValidator${extension}`)
+  const extension = process.platform === 'win32' ? '.exe' : '.dll'
+  return pathLikeToDirPath(resolve(import.meta.dirname, '../../../')).gotoFile(isDev() ? `../YARGReplayValidator/bin/Debug/net8.0/YARGReplayValidator${extension}` : `bin/YARGReplayValidator${extension}`)
 }
