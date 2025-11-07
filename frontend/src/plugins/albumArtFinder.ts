358abdf24cf2053366823e88066bad3a10aee110
Raphael Goulart <contact@raphaelgoul.art>
2025-11-07 18:55:58 -0300

    frontend: use enchorus API for album art first

--- frontend/src/plugins/albumArtFinder.ts
+++ frontend/src/plugins/albumArtFinder.ts
@@ -1,8 +1,28 @@
 import axios from 'axios'
 
-export async function albumArtFinder(artist: string, album?: string) {
+export async function albumArtFinder(
+  name: string,
+  artist: string,
+  charter?: string,
+  album?: string,
+) {
+  const regex = /(<color="?#?\w+"?>)|(<\/color>)/g
+  name = name.replace(regex, '')
+  artist = artist.replace(regex, '')
+  if (charter) charter = charter.replace(regex, '')
+  if (album) album = album.replace(regex, '')
+  if (name && artist && charter) {
+    try {
+      const result = await axios.post('https://api.enchor.us/album-art', {
+        name,
+        artist,
+        charter,
+      })
+      return result.data
+    } catch {}
+  }
   if (!artist || !album) return
-  return await axios.get('https://ws.audioscrobbler.com/2.0/', {
+  const result = await axios.get('https://ws.audioscrobbler.com/2.0/', {
     params: {
       method: 'album.getInfo',
       format: 'json',
@@ -11,4 +31,7 @@ export async function albumArtFinder(artist: string, album?: string) {
       api_key: import.meta.env.VITE_LAST_FM_API_KEY,
     },
   })
+  if (result && result.data) {
+    if (result.data.album.image[2]['#text']) return result.data.album.image[2]['#text']
+  }
 }
